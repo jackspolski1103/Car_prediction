33f16238fe87230b1c7d7ce12f51b4c6e3fdaf80
Julizimmerman <jzimmerman@udesa.edu.com>
2024-06-21 18:42:59 -0300

    cotizacion dolar

--- src/pre_procesado/precio/procesar_precio.py
+++ src/pre_procesado/precio/procesar_precio.py
@@ -1,9 +1,9 @@
 import numpy as np 
 import pandas as pd
 
-def preprocesar_precio(df):
+def preprocesar_precio(df, dolar):
     # fecha de bajada del dataset: 9/05/2024 
-    # cotizacion del dolar: 900 
+    # cotizacion del dolar: 1030
 
     monedas = df["Moneda"].copy()
     precios = df["Precio"].values
@@ -11,7 +11,7 @@ def preprocesar_precio(df):
         if pd.isnull(precios[p]) or precios[p] is pd.NA:  
             precios[p] = 0
         if monedas[p] == "$":
-            precios[p] = precios[p] / 900
+            precios[p] = precios[p] / dolar  
             # le dejo solo 4 decimales
             precios[p] = round(precios[p], 4)
             monedas[p] = "U$S" 
